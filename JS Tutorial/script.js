d3851e2094db08301e5349a5ef397b113b4bb517
Tejas Singhal <tjssinghal@icloud.com>
2024-12-12 10:00:50 -0500

    KeyLogger App

---
+++ JS Tutorial/script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+
+let myfunc = (div) => {
+    div.style.backgroundColor = "red";
+    
+    
+};
+
+
+let myfunc2 = (div) => {
+    div.style.backgroundColor = "purple";
+    
+    
+};
+
+
+
+
+
---
+++ KeyLogger App (HTML CSS JS)/script.js
@@ -0,0 +1,35 @@
+const logDiv = document.getElementById("log");
+const stateDiv = document.getElementById("state");
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+
+
+
+startBtn.addEventListener('click', ()=>{
+    document.addEventListener('keydown', handleDown);
+    document.addEventListener('keyup', handleUp);
+    
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', ()=>{
+    document.removeEventListener('keydown', handleDown);
+    document.removeEventListener('keyup', handleUp);
+    logDiv.textContent = "";
+    stateDiv.textContent = "";
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
+
+
+let handleDown = (e) => {
+    logDiv.textContent = `Key ${e.key} pressed down`;
+    stateDiv.textContent = "Key is down";
+};
+
+let handleUp = (e) => {
+    logDiv.textContent = `Key ${e.key} is up`;
+    stateDiv.textContent = "Key is up";
+};
